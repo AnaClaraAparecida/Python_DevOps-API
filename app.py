10977f7a7033209647c530d93f09931a5c8a2d93
Ana Clara Aparecida <anaclara.aparecida.027@gmail.com>
2025-12-02 10:10:51 -0300

    update

--- app.py
+++ app.py
@@ -20,6 +20,20 @@ def delete_by_username(username):
     db.users.delete_one({'username':username})
     return flask.jsonify({'ACK': 'Usuario removido com sucesso'}) 
 
+@app.route("/users/update", methods = ["PUT"])
+def update_user():
+    db = get_conn('pessoa')
+
+    user = dict(flask.requets.json)
+
+    user_to_update = db.users.find({'username': user.get('username')})
+
+    if user_to_update.count() != 1: 
+        return flask.jsonify({'NOK': 'Usuario não encontrado'}), 400
+
+    db.users.update_one({'username' : user.get('username')}, {"$set" : user })
+    return flask.jsonify({'ACK': 'Usuario atualizado com sucesso', 'data': user }), 200
+
 @app.route("/users/insert", methods=["POST"])
 def add_user():
 
